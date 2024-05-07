500cdeec3a349f3419956d2e9447b2542e96ada2
abhishek2002s <linkedin23356abhi@gmail.com>
2024-05-07 06:02:21 +0530

    ejs

--- ExpressJS/FormHandling/script.js
+++ ExpressJS/FormHandling/script.js
@@ -5,6 +5,8 @@
 const express = require('express');
 const app = express();
 
+
+//in do code ka use blob text ko readable form me convert krne ke liye use kiya jata hai
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
@@ -14,7 +16,7 @@ app.get("/",function(req,res){
 
 app.get("/about", function(req,res){
     res.send("about page hai");
-})
+});
 
 app.get("/profile",function(req,res,next){
     return next(new Error("Not implemented"));
@@ -23,5 +25,9 @@ app.get("/profile",function(req,res,next){
 app.listen(3000);
 
 
-//session 
-//cookie
+//session  - jab aap login rhte ho kisi webpage per tabhi tak us per kaam kar sakte ho isi cheej ko hum session khte hai 
+
+// express-session is a middleware module in Express. js that allows you to create sessions in your web application. It stores session data on the server side, using a variety of different storage options, and allows you to track the activity of a user across requests
+
+
+//cookie - jab app kisi web server per jaate ho or login krte ho tab aap ko remember krne ke liye serve apko ek string deta wo string hi appka cookies kehlata hai
